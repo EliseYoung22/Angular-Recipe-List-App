1b79d969e31a60e7373ec4aaf78953fc7ce6987d
Elise Young <eliseyoung_4@hotmail.com>
2017-08-21 08:54:24 +1000

    add shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,12 @@
-export class ShoppingListService {
+import { Ingredient } from '../shared/ingredient.model';
 
+export class ShoppingListService {
+    private ingredients: Ingredient[] = [
+        new Ingredient('potatoes', 5),
+        new Ingredient('bananas', 5)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
 }
